4ef4f5dfd02560d81f867feb2fbc6bab00d99d92
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-08-03 17:28:06 +0100

    stress test

--- app.go
+++ app.go
@@ -1,6 +1,41 @@
 package main
 
 import (
+	"fmt"
+	electron "./electronbridge_golib"
+)
+
+const (
+	WIDTH = 40
+	HEIGHT = 30
+)
+
+func main() {
+	main_window := electron.NewGridWindow("World", "pages/grid.html", WIDTH, HEIGHT, 12, 20, 100, true)
+	text_window := electron.NewTextWindow("Text", "pages/log.html", 400, 300, true)
+
+	i := 0
+
+	for {
+		i++
+
+		s := fmt.Sprintf("%d", i)
+
+		for x := 0; x < len(s); x++ {
+			main_window.Set(x + 1, 1, string(s[x]), "g")
+		}
+
+		main_window.Flip()
+
+		if i % 50 == 0 {
+			text_window.Printf("Reached i %d\n", i)
+		}
+	}
+}
+
+/*
+
+import (
 	"math/rand"
 	"time"
 	electron "./electronbridge_golib"
@@ -33,3 +68,5 @@ func main() {
 		time.Sleep(50 * time.Millisecond)
 	}
 }
+
+*/
